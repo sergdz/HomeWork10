f8357955c5cdedf039ae56425aadd8a79db2846c
sergdz <sergeydzubenko1@gmail.com>
2022-04-28 18:59:39 +0300

    master

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const arr = [1,2,3,4,5,6,7,8,9,10];
-const result = [];
 function chunk(arr, chunkSize ) {
-    
+  const result = [];
     for (let i = 0; i < arr.length; i++) {
         result.push(arr.slice(i, chunkSize + i));
       i += chunkSize - 1;
